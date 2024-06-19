d310cc6f2d5b75916d7e75935985d2099434d48e
tunk <nguyenkhactu1996@gmail.com>
2024-06-19 16:04:03 +0700

    UserInfoResponse

---
+++ api-common/src/main/java/vn/edu/vti/vmall/api/common/payload/service/user/response/UserInfoResponse.java
@@ -0,0 +1,14 @@
+package vn.edu.vti.vmall.api.common.payload.service.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoResponse {
+  private String phone;
+}
